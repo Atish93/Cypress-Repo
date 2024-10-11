b97c6f411b2910f6004f5fd24fb9805463d448ad
Atish93 <rathod.atish444@gmail.com>
2024-10-11 17:41:42 +0530

    Feat: added test cases for top navigation bar

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,19 +1,33 @@
-import { logo, searchEngineLocator } from "../constants/homePage";
+import { barItems, logo, searchEngineLocator } from "../constants/homePage";
 
-describe("template spec", () => {
-  beforeEach(() => {
+describe("Home Page Cases", () => {
+  it("Visite Page", () => {
     cy.visit("/");
   });
 
+  // Check Logo 
   it("passes", () => {
-    cy.get(logo).should("be.visible");
+    cy.get(logo).eq(0).should("be.visible");
   });
 
+  // Check Search Engine
   it("Check Search Engine", () => {
     cy.get(searchEngineLocator).should("be.visible");
   });
 
+  // Type and check Dropdown on search engine
   it("Type in Search Engine", () => {
-    cy.get(searchEngineLocator).type("mobile").should("have.value", "mobile");
+    cy.get(searchEngineLocator).type("Java").should("have.value", "Java");
   });
+
+  // Just check Dropdown is opened
+  it('Check Dropdown Options', ()=>{
+    cy.get('#tnb-search-suggestions').should('have.attr', 'style', 'display: block;');    
+  })
+
+  it('Verify Top Nav Items', ()=>{
+    barItems.forEach((item)=>{
+      cy.get(item.selector).should('contain.text', item.text).and('be.visible');
+    })
+  })
 });
